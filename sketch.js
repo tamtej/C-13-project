435b097e78336d504a7b71807ed59c2e573f6b2d
tamtej <85797462+tamtej@users.noreply.github.com>
2021-08-02 18:37:52 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,79 @@
+var garden,rabbit,apple,orangeL,redL;
+var gardenImg,rabbitImg,carrotImg,orangeImg,redImg
+
+function preload(){
+  gardenImg = loadImage("garden.png");
+  rabbitImg = loadImage("rabbit.png");
+  appleImg = loadImage("apple.png");
+  orangeImg = loadImage("orangeLeaf.png");
+  redImg = loadImage("redImage.png");
+}
+
+function setup(){
+  
+  createCanvas(400,400);
+  
+// Moving background
+garden=createSprite(200,200);
+garden.addImage(gardenImg);
+
+//creating rabbit
+rabbit = createSprite(180,340,30,30);
+rabbit.scale =0.09;
+rabbit.addImage(rabbitImg);
+}
+
+
+//rabbit moving on Xaxis with mouse
+rabbit.x=World.mouseX;
+
+drawSprites()
+var select_sprites = Math.round(random(1,3));
+
+if (frameCount % 80 ==0) {
+  if (select_sprites == 2) {
+    createApples();
+  }else if (select_sprites == 2) {
+    createOrange();
+  }else {
+    createRed();
+  }
+}
+
+function createApples(){
+  apple =createSprite(random(50,350),40,10,10)
+apple.addImage(appleImg);
+apple.scale=0.07;
+apple.velocityY=3;
+apple.lifetime=150;
+
+}
+
+function createOrange(){
+  orangeL =createSprite(random(50,350),40,10,10)
+orangeL.addImage(orangeImg);
+orangeL.scale=0.08;
+orangeL.velocityY=3;
+orangeL.lifetime=150;
+
+}
+
+function createRed(){
+  redL =createSprite(random(50,350),40,10,10)
+  redL.addImage(orangeImg);
+redL.scale=0.08;
+redL.velocityY=3;
+redL.lifetime=150;
+
+}
+
+
+function draw() {
+  background(0);
+  
+  
+  edges= createEdgeSprites();
+  rabbit.collide(edges);
+
+  
+}
